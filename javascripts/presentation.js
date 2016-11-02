82c98d28dbbbb089ef6f6a38edf86df15c446631
Thomas ZILLIOX <thomas@zilliox.me>
2016-11-02 19:57:06 +0100

    Enable touch navigation

--- javascripts/presentation.js
+++ javascripts/presentation.js
@@ -2,6 +2,7 @@
 var deck = bespoke.from('article', [
     bespoke.plugins.classes(),
     bespoke.plugins.keys(),
+    bespoke.plugins.touch(),
     bespoke.plugins.hash(),
     bespoke.plugins.slidenumber()
 ]);
